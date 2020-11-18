0dc1438a553cf87313411c5f501c294b0c463a35
Camilo-UbuntuPC <camilo.jara@ug.uchile.cl>
2020-11-18 02:10:41 -0300

    adding create virtual channel method

--- src/utils/re_referencing.py
+++ src/utils/re_referencing.py
@@ -1,5 +1,9 @@
 from typing import Callable, Dict, List, Optional, Tuple, Union
 
+import numpy as np 
+
+from .filters import filter_band_array_to_array
+
 import mne
 from mne.io.edf.edf import RawEDF
 
@@ -38,3 +42,17 @@ def rref_REST(raw: RawEDF) -> RawEDF:
     forward = mne.make_forward_solution(raw.info, trans=None, src=src, bem=sphere, verbose=False)
     raw.set_eeg_reference('REST', forward=forward, verbose=False)
     return raw
+
+def create_virtual_channel(raw: RawEDF, chs_names: List) -> List:
+    """
+    Create a virtual channel from chs_names
+    """
+    chs_data = [raw[ch_name][0] for ch_name in chs_names]
+    ocular_data = np.mean(np.concatenate(chs_data), axis=0)
+    ocular_virtual = filter_band_array_to_array(
+        data=ocular_data, 
+        band=[0.1, 5], 
+        fs=raw.info["sfreq"], 
+        order=2
+    )
+    return ocular_virtual
