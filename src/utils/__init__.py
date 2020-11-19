e40b576ffb44e2a496a10231cdc81e482fcd1688
Camilo-UbuntuPC <camilo.jara@ug.uchile.cl>
2020-11-19 04:52:01 -0300

    renaming imports

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,15 +1,17 @@
-from .filters import filter_band_array_to_array
-from .filters import filter_band_raw_to_array
-from .filters import filter_band_raw_to_raw
-from .filters import create_windows
-from .filters import eliminate_blink_corr_electrodes
-from .filters import create_windows
+from .read_data import read_data
+
+from .data_filtering import filter_band_array_to_array
+from .data_filtering import filter_band_raw_to_array
+from .data_filtering import filter_band_raw_to_raw
+from .data_filtering import create_windows
+from .data_filtering import eliminate_blink_corr_electrodes
+from .data_filtering import create_windows
+from .data_filtering import clean_windows_artifacts
+from .data_filtering import reconstruct_signal
 
 from .re_referencing import rref_remove
 from .re_referencing import rref_average
 from .re_referencing import rref_CSD
 from .re_referencing import rref_REST
 from .re_referencing import create_virtual_channel
-
-from .read_data import read_data
 
