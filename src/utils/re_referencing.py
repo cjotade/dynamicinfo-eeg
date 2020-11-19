3a8bb52fbdbce7d54e1dfd9f5f367ead1772e96a
Camilo-UbuntuPC <camilo.jara@ug.uchile.cl>
2020-11-19 04:50:49 -0300

    sorting import and fixing circular imports in function

--- src/utils/re_referencing.py
+++ src/utils/re_referencing.py
@@ -1,11 +1,9 @@
 from typing import Callable, Dict, List, Optional, Tuple, Union
 
-import numpy as np 
-
-from .filters import filter_band_array_to_array
-
 import mne
+import numpy as np
 from mne.io.edf.edf import RawEDF
+
 
 def rref_remove(raw: RawEDF) -> RawEDF:
     raw_rref = raw.copy()
@@ -47,6 +45,9 @@ def create_virtual_channel(raw: RawEDF, chs_names: List) -> List:
     """
     Create a virtual channel from chs_names
     """
+    from .data_filtering import filter_band_array_to_array
+    
+    raw = raw.copy()
     chs_data = [raw[ch_name][0] for ch_name in chs_names]
     ocular_data = np.mean(np.concatenate(chs_data), axis=0)
     ocular_virtual = filter_band_array_to_array(
